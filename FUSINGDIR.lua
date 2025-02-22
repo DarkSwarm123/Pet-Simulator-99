857a55f7b063670b62ab86651f137fa13d531484
DarkSwarm123 <minecraftmodfandarkswarm@gmail.com>
2025-02-22 10:53:35 +0100

    Update FUSINGDIR.lua

--- FUSINGDIR.lua
+++ FUSINGDIR.lua
@@ -1,5 +1,5 @@
 return {
-    [1] = {["c20cde7f3c1c4bab9ca538485e433bde"] = 3},
+    [1] = {["c20cde7f3c1c4bab9ca538485e433bde"] = 100},
     [2] = {["7997b1421a5745dcae4e88cfd36b082a"] = 100},
     [3] = {["94b2999bccf94acd81dd9fbdf31cfa6b"] = 100},
     [4] = {["20bd2e4cf55f42a89cd8b22be26d6a54"] = 100},
